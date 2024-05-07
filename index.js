1c46145b896936157270d95ce6d58bf0ebe696fa
wchon <wangchau31@gmail.com>
2024-05-07 00:45:38 -0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'upload/' });
 
 var app = express();
 
@@ -11,6 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let upfile = req.file;
+  if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+  try {
+    res.json({
+      name: upfile.originalname,
+      type: upfile.mimetype,
+      size: upfile.size,
+    });
+  } catch (err) {
+    res.send(400);
+  }
+  
+});
 
 
 
